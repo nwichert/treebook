d2bea5daeaff47b6bb4b3361e2b26a556ab1b2a0
Nick Wichert <nick.wichert@icloud.com>
2014-06-16 12:40:34 -0500

    Modified add_user_id_to_statuses.rb

--- db/migrate/20140616170443_add_user_id_to_statuses.rb
+++ db/migrate/20140616170443_add_user_id_to_statuses.rb
@@ -1,7 +1,7 @@
 class AddUserIdToStatuses < ActiveRecord::Migration
   def change
-  	add_column :status, :user_id, :integer
-  	add_index :status, :user_id
-  	remove_column :status, :name
+  	add_column :statuses, :user_id, :integer
+  	add_index :statuses, :user_id
+  	remove_column :statuses, :name
   end
 end
